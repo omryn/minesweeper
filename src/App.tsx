b0f93b86a8d361d16a78ba3dc57a48aaedc733c4
Omry Nachman <omry.nachman@walkme.com>
2021-05-26 10:05:31 +0300

    Made timer reactive to game state

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ function App() {
   const [height, setHeight] = useState(20);
   const [mines, setMines] = useState(10);
   const [counter, setCounter] = useState(0);
+  const [active, setActive] = useState(false) 
   const reset = () => setCounter(counter+1);
 
   return (
     <div className="App">
-      <Header {...{width, height, mines, setWidth, setHeight, setMines}} onReset={reset} timerId={counter}/>
+      <Header {...{width, height, mines, setWidth, setHeight, setMines}} onReset={reset} timerId={counter} timerActive={active}/>
       <main>
-        <Board width={width} height={height} minesProbability={mines / 100} key={counter} />
+        <Board width={width} height={height} minesProbability={mines / 100} key={counter} onActiveChanged={setActive} />
       </main>
     </div>
   );
