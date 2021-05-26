090b98d6205cb87f7c06f7ff02473ace74325a00
Omry Nachman <omry.nachman@walkme.com>
2021-05-26 08:45:39 +0300

    - Added timer placeholder - updated header extraction  to match phase3

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,19 @@
 import "./App.scss";
 import { Board } from "components/Board";
+import { Header } from "components/Header";
 import { useState } from "react";
 function App() {
   const [width, setWidth] = useState(30);
   const [height, setHeight] = useState(20);
   const [mines, setMines] = useState(10);
   const [counter, setCounter] = useState(0);
+  const reset = () => setCounter(counter+1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Best mine sweeper ever</h1>
-        <div className="header__inputs">
-          <label htmlFor="width">Width</label>
-          <input
-            id="width"
-            type="number"
-            value={width}
-            min="3"
-            max="300"
-            step="1"
-            onChange={({ target }) => setWidth(parseInt(target.value))}
-          />
-          <label htmlFor="height">Height</label>
-          <input
-            id="height"
-            type="number"
-            value={height}
-            min="3"
-            max="300"
-            step="1"
-            onChange={({ target }) => setHeight(parseInt(target.value))}
-          />
-          <label htmlFor="mines">Mines %</label>
-          <input
-            id="mines"
-            type="number"
-            value={mines}
-            min="1"
-            max="100"
-            step="1"
-            onChange={({ target }) => setMines(parseInt(target.value))}
-          />
-          <button className="header__reset-button" onClick={() => setCounter(counter ? 0 : 1)} />
-        </div>
-      </header>
+      <Header {...{width, height, mines, setWidth, setHeight, setMines}} onReset={reset} timerId={counter}/>
       <main>
-        <Board
-          width={width}
-          height={height}
-          minesProbability={mines / 100}
-          key={counter}
-        />
+        <Board width={width} height={height} minesProbability={mines / 100} key={counter} />
       </main>
     </div>
   );
---
+++ src/components/Timer.tsx
@@ -0,0 +1,5 @@
+import "./Timer.scss";
+
+export const Timer = () => {
+  return <div className="timer">00:00</div>;
+};
