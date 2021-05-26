a56b0ae714c8314929c245c65c2343be21eec123
Omry Nachman <omry.nachman@walkme.com>
2021-05-26 08:45:39 +0300

    - Added timer placeholder - updated header extraction  to match phase3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,42 @@
 import "./Header.scss";
 import { Timer } from "components/Timer";
+import { ChangeEvent } from "react";
+
+type ChangeHandler = (value: number) => void;
 export interface HeaderProps {
   height: number;
   width: number;
   mines: number;
+  setMines: ChangeHandler;
+  setHeight:ChangeHandler;
+  setWidth: ChangeHandler;
+  onReset: () => void;
+  timerId:number
 }
-export const Header = ({ width, height, mines }: HeaderProps) => (
+const handleWith = (fn:ChangeHandler) => (e:ChangeEvent<HTMLInputElement>) => fn(Number(e.target.value))
+
+export const Header = ({
+  width,
+  height,
+  mines,
+  setMines,
+  setHeight,
+  setWidth,
+  onReset,
+  timerId
+}: HeaderProps) => (
   <header className="header">
     <h1>Best mine sweeper ever</h1>
     <div className="header__inputs">
       <label htmlFor="width">Width</label>
-      <input id="width" type="number" min="3" max="300" step="1" />
+      <input id="width" type="number" value={width} min="3" max="300" step="1" onChange={handleWith(setWidth)}/>
       <label htmlFor="height">Height</label>
-      <input id="height" type="number" min="3" max="300" step="1" />
+      <input id="height" type="number" value={height} min="3" max="300" step="1" onChange={handleWith(setHeight)}/>
       <label htmlFor="mines">Mines %</label>
-      <input id="mines" type="number" min="1" max="100" step="1" />
+      <input id="mines" type="number" value={mines} min="1" max="100" step="1" onChange={handleWith(setMines)}/>
       <label htmlFor="reset">New game</label>
-      <button id="reset" className="header__reset-button" />
-      <Timer />
+      <button id="reset" className="header__reset-button" onClick={onReset}/>
+      <Timer key={timerId}/>
     </div>
   </header>
 );
